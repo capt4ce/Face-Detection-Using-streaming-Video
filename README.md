9273f1e90933b42edc4045faeceafc6239f3d3cd
Ahmad Ali Abdilah <ahmad.ali.abdilah@gmail.com>
2018-01-21 08:24:14 +0530

    working model

---
+++ README.md
@@ -0,0 +1,7 @@
+# Face-Detection-Using-streaming-Video
+Face Detection Using streaming video from a phone through wifi. IpWebcam android app is used to interface this operation. This implementation is useful to make a prototype of Realtime Inspection Support Drone.
+
+The method used in this project is just a simple method HAAR-Cascade that will detect the front structure of the face.
+
+Base tutorial:
+https://realpython.com/blog/python/face-detection-in-python-using-a-webcam/
---
+++ detect.py
@@ -0,0 +1,63 @@
+# Stream Video with OpenCV from an Android running IP Webcam (https://play.google.com/store/apps/details?id=com.pas.webcam)
+# Code Adopted from http://stackoverflow.com/questions/21702477/how-to-parse-mjpeg-http-stream-from-ip-camera
+
+# import the necessary packages
+import numpy as np
+import cv2
+import urllib2
+import sys
+
+host = "192.168.43.1:8080"
+# host = "172.16.164.179:8080"
+
+if len(sys.argv)>1:
+    host = sys.argv[1]
+
+hoststr = 'http://' + host + '/video'
+print 'Streaming ' + hoststr
+
+stream=urllib2.urlopen(hoststr)
+
+cascPath = "haarcascade_frontalface_default.xml" #sys.argv[1]
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+capturedNo = 0
+
+bytes=''
+while True:
+    status = "No Targets"
+    bytes+=stream.read(1024)
+    a = bytes.find('\xff\xd8')
+    b = bytes.find('\xff\xd9')
+    if a!=-1 and b!=-1:
+        jpg = bytes[a:b+2]
+        bytes= bytes[b+2:]
+        frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),1)#cv2.CV_LOAD_IMAGE_COLOR)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE #cv.CV_HAAR_SCALE_IMAGE
+        )
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # capturing the detected face
+        capturedNo += 1
+        print capturedNo
+        cv2.imwrite("frame"+str(capturedNo)+".bmp", frame)
+
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
